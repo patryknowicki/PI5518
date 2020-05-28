89cbf1dedd5631c3d664ab5d0c0fed14b5dac293
Patryk Nowicki <patryk.nowicki29@gmail.com>
2020-05-28 13:48:32 +0200

    db

--- src/main/java/pl/pi5518/aplikacja/repository/PhonesRepo.java
+++
@@ -1,9 +0,0 @@
-package pl.pi5518.aplikacja.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.pi5518.aplikacja.databases.Phones;
-
-public interface PhonesRepo extends JpaRepository<Phones, Integer> {
-}
-
-
