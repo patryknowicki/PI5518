80bb131fe1c9bd270959af355a3f485922170023
Patryk Nowicki <patryk.nowicki29@gmail.com>
2020-10-26 20:05:41 +0100

    ctrll

---
+++ src/main/java/pl/pi5518/aplikacja/controller/DbController.java
@@ -0,0 +1,95 @@
+package pl.pi5518.aplikacja.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pi5518.aplikacja.databases.Notebooks;
+import pl.pi5518.aplikacja.databases.Pcs;
+import pl.pi5518.aplikacja.databases.Tablets;
+import pl.pi5518.aplikacja.repository.NotebooksRepo;
+import pl.pi5518.aplikacja.repository.PcsRepo;
+import pl.pi5518.aplikacja.repository.TabletsRepo;
+
+@Controller
+public class DbController {
+    private NotebooksRepo notebooksRepo;
+    private TabletsRepo tabletsRepo;
+    private PcsRepo pcsRepo;
+
+    @Autowired
+    public DbController(NotebooksRepo notebooksRepo, TabletsRepo tabletsRepo, PcsRepo pcsRepo) {
+        this.notebooksRepo = notebooksRepo;
+        this.tabletsRepo = tabletsRepo;
+        this.pcsRepo = pcsRepo;
+    }
+
+    @GetMapping("/newNotebook")
+    public String showFormAddNotebook(Model model) {
+        Notebooks notebooks = new Notebooks();
+        model.addAttribute("newNotebook", notebooks);
+        return "add-notebooks";
+    }
+
+    @GetMapping("/newTablet")
+    public String showFormAddTablet(Model model) {
+        Tablets tablets = new Tablets();
+        model.addAttribute("newTablet", tablets);
+        return "add-tablets";
+    }
+
+    @GetMapping("/newPc")
+    public String showFormAddPc(Model model) {
+        Pcs pcs = new Pcs();
+        model.addAttribute("addPc", pcs);
+        return "add-pcs";
+    }
+
+    @PostMapping("/newNotebook")
+    public String addNotebooks(@ModelAttribute("newNotebook") Notebooks notebooks) {
+        notebooksRepo.save(notebooks);
+        return "redirect:/list-notebooks";
+    }
+
+    @PostMapping("/newTablet")
+    public String addTablets(@ModelAttribute("newTablet")Tablets tablets){
+        tabletsRepo.save(tablets);
+        return "redirect:/list-tablets";
+    }
+
+    @PostMapping("/newPc")
+    public String addPc(@ModelAttribute("newPc")Pcs pcs){
+        pcsRepo.save(pcs);
+        return "redirect:/list-pcs";
+    }
+    @RequestMapping("/list-notebooks")
+    public String listNotebooks(Model model) {
+
+        for (Notebooks notebooks : notebooksRepo.findAll()) {
+            System.out.println(notebooks);
+        }
+        model.addAttribute("notebooks", notebooksRepo.findAll());
+        return "list-notebooks";
+    }
+
+    @RequestMapping("/list-tablets")
+    public String listTablets(Model model){
+        for (Tablets tablets : tabletsRepo.findAll()){
+            System.out.println(tablets);
+        }
+        model.addAttribute("tablets", tabletsRepo.findAll());
+        return "list-tablets";
+    }
+
+    @RequestMapping("/list-pcs")
+    public String listPcs(Model model){
+        for (Pcs pcs : pcsRepo.findAll()){
+            System.out.println(pcs);
+        }
+        model.addAttribute("pcs", pcsRepo.findAll());
+        return "list-pcs";
+    }
+}
