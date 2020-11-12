2263c2540765bf91e06c0f08a022ef28d85a88c3
Patryk_PC <patryk.nowicki29@gmail.com>
2020-11-12 21:51:05 +0100

    cls

--- src/main/java/pl/pi5518/aplikacja/controller/TokenController.java
+++ src/main/java/pl/pi5518/aplikacja/controller/TokenController.java
@@ -1,26 +1,18 @@
 package pl.pi5518.aplikacja.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import pl.pi5518.aplikacja.dto.AppUserDto;
 import pl.pi5518.aplikacja.model.AppUser;
 import pl.pi5518.aplikacja.model.Token;
 import pl.pi5518.aplikacja.repository.*;
-import pl.pi5518.aplikacja.service.UserService;
-
-import javax.validation.Valid;
 
 @Controller
 public class TokenController {
 
-    private UserService userService;
     private TokenRepo tokenRepo;
     private AppUserRepo appUserRepo;
 
-    public TokenController(UserService userService, TokenRepo tokenRepo, AppUserRepo appUserRepo) {
-        this.userService = userService;
+    public TokenController(TokenRepo tokenRepo, AppUserRepo appUserRepo) {
         this.tokenRepo = tokenRepo;
         this.appUserRepo = appUserRepo;
     }
